67a419885f762f6ff6cc3373d7f01c9b044b6a73
gurung69 <gurungkrish44@gmail.com>
2025-04-11 15:55:48 -0600

    implemented game setup and game logic

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
+import wordChains from './static/wordchian'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const wordChain = wordChains[0]
+  const [guess, setGuess] = useState('')
+  const [wordIndex, setWordIndex] = useState(1)
+  
+  const words = wordChain.map((word, index)=>{
+    if (index === 0){
+      return (<li key={index}>{(word[0].toUpperCase() + word.slice(1)).split("").join(" ")}</li>)
+    }
+    return(
+      <li key={index}>{word[0].toUpperCase() + " _".repeat(word.length - 1)}</li>
+    )
+  })
+
+  function checkWin(){
+    if (wordIndex === wordChain.length){
+      alert("You Win!")
+    }
+  }
+
+  function checkGuess(){
+    if (wordChain[wordIndex].toLowerCase() === guess.toLowerCase()){
+      const wordList = document.getElementsByTagName('li')[wordIndex]
+      wordList.textContent = (wordChain[wordIndex][0].toUpperCase() + wordChain[wordIndex].slice(1)).split("").join(" ")
+      wordList.classList.add("text-green-500")
+      setWordIndex(wordIndex + 1)
+    }
+    else{
+      const wordList = document.getElementsByTagName('li')[wordIndex]
+      const visibleWord = wordList.textContent.replace(/[_\s]/g, "");
+      wordList.textContent = (wordChain[wordIndex][0].toUpperCase() + " " + wordChain[wordIndex].slice(1, visibleWord.length+1).split("").join(" ") + " _".repeat(wordChain[wordIndex].length - visibleWord.length - 1))
+    }
+  }
+
+  function handleSumbit(e){
+    e.preventDefault()
+    checkGuess()
+  }
+  
+  useEffect(()=>{
+    checkWin()
+  }, [wordIndex])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ul>
+        {words}
+      </ul>
+      <form onSubmit={handleSumbit}>
+        <input type="text" value={guess}
+        onChange={e=>setGuess(e.target.value)}
+        className='border-1'/>
+      </form>
     </>
   )
 }
